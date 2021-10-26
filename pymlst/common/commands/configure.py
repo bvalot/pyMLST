0c19dee39d98a24664043a7461cc2783cc603643
Benoit Valot <benoit.valot@univ-fcomte.fr>
2021-10-26 13:08:30 +0200

    Add kma executable to config

--- pymlst/common/commands/configure.py
+++ pymlst/common/commands/configure.py
@@ -10,6 +10,9 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--blat', '-b',
               type=click.Path(exists=True, dir_okay=False),
               help='Blat executable absolute path.')
+@click.option('--kma', '-k',
+              type=click.Path(exists=True, dir_okay=False),
+              help='Kma executable absolute path.')
 @click.option('--mafft', '-m',
               type=click.Path(exists=True, dir_okay=False),
               help='Mafft executable absolute path.')
@@ -19,17 +22,19 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option('--reset', '-r',
               is_flag=True,
               help='Reset the configuration.')
-def cli(blat, mafft, log, reset):
+def cli(blat, kma, mafft, log, reset):
     """Configure executables paths and log level."""
     if reset:
         config.reset_binary_paths()
         config.set_logging_level("INFO")
         click.echo('Resetting the configuration...')
 
-    if mafft or blat:
+    if mafft or blat or kma:
         paths = {}
         if blat:
             paths['blat'] = blat
+        if kma:
+            paths['kma'] = kma
         if mafft:
             paths['mafft'] = mafft
         config.update_binary_paths(paths)
